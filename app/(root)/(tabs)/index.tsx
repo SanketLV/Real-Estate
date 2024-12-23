1b3462a0df18c098593b5bafa056137889632187
SanketLV <sanketlakhani80@gmail.com>
2024-12-23 13:54:31 +0530

    bottom navigation bar added

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -3,12 +3,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="">
+    <View className="flex-1 justify-center items-center">
       <Text className="font-rubik text-3xl">Welcome to ReState</Text>
-      <Link href="/sign-in">Sign-in</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Property</Link>
     </View>
   );
 }
